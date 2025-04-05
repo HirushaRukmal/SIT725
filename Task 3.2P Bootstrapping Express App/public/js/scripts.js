4b42d507342016fdf092decbf2a3867a86fd802d
HirushaRukmal <hirusharukmal266@gmail.com>
2025-04-06 01:12:22 +1100

    Week 3

--- Task 3.2P Bootstrapping Express App/public/js/scripts.js
+++ Task 3.2P Bootstrapping Express App/public/js/scripts.js
@@ -1,65 +1,68 @@
 const cardList = [
     {
-        title: "Kitten 2",
-        image: "image/kitten2.png",
-        link: "About Kitten 2",
-        description: "A cute little kitten!"
+        title: "Doggie 2",
+        image: "images/dog2.jpeg",
+        link: "About Dog 2",
+        desciption: "Demo desciption about Dog 2",
     },
     {
-        title: "Kitten 3",
-        image: "image/kitten3.png",
-        link: "About Kitten 3",
-        description: "An adorable fluffy kitten!"
-    }
+        title: "Doggie 3",
+        image: "images/dog3.jpeg",
+        link: "About Dog 3",
+        desciption: "Demo desciption about Dog 3",
+    },
 ];
 
-// Function to handle button click
 const clickMe = () => {
     alert("Thanks for clicking me. Hope you have a nice day!");
 };
 
-// Function to add cards dynamically
 const addCards = (items) => {
-    console.log("Adding new cards..."); // Debugging step
-    items.forEach(item => {
-        let itemToAppend = `
-            <div class="col s4">
-                <div class="card medium">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="${item.image}">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">
-                            ${item.title}<i class="material-icons right">more_vert</i>
-                        </span>
-                        <p><a href="#">${item.link}</a></p>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">
-                            ${item.title}<i class="material-icons right">close</i>
-                        </span>
-                        <p>${item.description}</p>
-                    </div>
-                </div>
-            </div>`;
-
-        // Append new card
+    items.forEach((item) => {
+        let itemToAppend =
+            '<div class="col s4 center-align">' +
+            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' +
+            item.image +
+            '">' +
+            '</div><div class="card-content">' +
+            '<span class="card-title activator grey-text text-darken-4">' +
+            item.title +
+            '<i class="material-icons right">more_vert</i></span><p><a href="#">' +
+            item.link +
+            "</a></p></div>" +
+            '<div class="card-reveal">' +
+            '<span class="card-title grey-text text-darken-4">' +
+            item.title +
+            '<i class="material-icons right">close</i></span>' +
+            '<p class="card-text">' +
+            item.desciption +
+            "</p>" +
+            "</div></div></div>";
         $("#card-section").append(itemToAppend);
     });
-
-    console.log("Cards added successfully!"); // Debugging step
 };
 
-// jQuery ready function to ensure DOM is loaded
 $(document).ready(function () {
-    console.log("Document ready!"); // Debugging step
-
-    $('.materialboxed').materialbox();
-
-    // Attach event listener
-    $('#clickMe').click(() => {
-        console.log("Button clicked!"); // Debugging step
-        clickMe();
+    $(".materialboxed").materialbox();
+    $("#clickMeButton").click(() => {
         addCards(cardList);
     });
+});
+
+const submitForm = () => {
+    let formData = {};
+    formData.first_name = $("#first_name").val();
+    formData.last_name = $("#last_name").val();
+    formData.password = $("#password").val();
+    formData.email = $("#email").val();
+
+    console.log("Form Data Submitted: ", formData);
+};
+
+$(document).ready(function () {
+    $(".materialboxed").materialbox();
+    $("#formSubmit").click(() => {
+        submitForm();
+    });
+    $(".modal").modal();
 });
